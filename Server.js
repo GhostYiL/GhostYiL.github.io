02e91541207ca38205b035777c097fbba4df0651
GhostYiL <51120128+GhostYiL@users.noreply.github.com>
2021-02-24 16:23:47 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,10 +1,11 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 })
+const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('ho!')
-})
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
