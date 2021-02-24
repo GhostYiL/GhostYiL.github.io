29fee5962d2a71469e1edb517df69c9230cd7e4d
GhostYiL <51120128+GhostYiL@users.noreply.github.com>
2021-02-24 18:46:27 +0300

    F

---
+++ public/client.js
@@ -0,0 +1,18 @@
+/* global FriendlyWebSocket */
+
+// check out websocket.js for what this is
+let s = new FriendlyWebSocket();
+
+s.on("message", data => {
+  console.log("message:", data);
+  try {
+    // this example expects every message to be in JSON format.
+    data = JSON.parse(event.data);
+  } catch (e) {
+    console.warn("invalid message from server", data);
+  }
+  // if it's a message about the client count, update the elements
+  if (data.type === "count") {
+    document.querySelector(".count").innerText = data.count;
+  }
+});
