a070dfad87e60641cec86ed76eddcf44634517a8
GhostYiL <51120128+GhostYiL@users.noreply.github.com>
2021-02-24 19:38:46 +0300

    up

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.sendFile(__dirname + "/view/index.html");
 });
 
 // listen for requests!
